ae9bf3ca7a55ea1c53563017ebbdbff154743e25
Haidong Wang <beavermelon@gmail.com>
2015-04-07 16:48:02 +0900

    init commit for contest reso

---
+++ src/main/java/contest/question1/Answer.java
@@ -0,0 +1,11 @@
+package contest.question1;
+
+/**
+ * Created by wang on 2015/04/07.
+ */
+public class Answer {
+
+    public int resolve() {
+        return 0;
+    }
+}
---
+++ src/test/java/contest/question1/AnswerTest.java
@@ -0,0 +1,7 @@
+package contest.question1;
+
+/**
+ * Created by wang on 2015/04/07.
+ */
+public class AnswerTest {
+}
