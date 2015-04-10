1c27abc77f73da0ee718be211728f81065b473f7
Haidong Wang <beavermelon@gmail.com>
2015-04-10 20:08:07 +0900

    temp commit for question 3

---
+++ src/main/java/contest/question3/Answer.java
@@ -0,0 +1,13 @@
+package contest.question3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wang on 2015/04/10.
+ */
+public class Answer {
+
+    protected static Logger log = LoggerFactory.getLogger(Answer.class);
+
+}
